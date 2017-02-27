4090d250c93861b81ebdf6af4b116577312e1db8
sk <sk@httplab.ru>
2017-02-27 17:49:00 +0300

    Fix AR exdending

--- lib/cursor/models/active_record_extension.rb
+++ lib/cursor/models/active_record_extension.rb
@@ -1,20 +1,18 @@
 require 'cursor/models/active_record_model_extension'
 
 module Cursor
-
-  module InheritedOverride
-    def inherited(kls)
-      inherited_without_cursor kls
-      kls.send(:include, Cursor::ActiveRecordModelExtension) if kls.superclass == ::ActiveRecord::Base
-    end
-  end
-
   module ActiveRecordExtension
     extend ActiveSupport::Concern
-    included do
-      # Future subclasses will pick up the model extension
-      self.prepend Cursor::InheritedOverride
 
+    module ClassMethods
+      # Future subclasses will pick up the model extension
+      def inherited(kls) #:nodoc:
+        super
+        kls.send(:include, Cursor::ActiveRecordModelExtension) if kls.superclass == ::ActiveRecord::Base
+      end
+    end
+
+    included do
       # Existing subclasses pick up the model extension as well
       self.descendants.each do |kls|
         kls.send(:include, Cursor::ActiveRecordModelExtension) if kls.superclass == ::ActiveRecord::Base
